113f69ae82a3f79657990c772440506491bc3247
DestinyOdia <odiadestiny56@gmail.com>
2024-02-19 12:19:01 -0500

    Merge commit 'bc7a118f3d59de9243cc0fb8f20b632c00d2ee2b' into Destiny_Odia_ICE1
    Conflicts:
    	src/card/CardTrick.java

--- src/ca/sheridancollege/project/GoFishPlayers.java
+++ src/ca/sheridancollege/project/GoFishPlayers.java
@@ -16,7 +16,8 @@ public class GoFishPlayers extends Player {
 
     @Override
     public void play() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        GoFishGame gaming= new GoFishGame();
+        gaming.play();
+        }
     
 }
